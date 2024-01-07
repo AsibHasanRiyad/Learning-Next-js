99b10eb86d8625fbb3f3a9fba176b4f2102637ef
Sayad Riyad <sayadriyad@Sayads-MacBook-Air.local>
2024-01-07 12:21:22 +0600

    image optimization

--- src/app/(mainLayout)/album/page.js
+++ src/app/(mainLayout)/album/page.js
@@ -1,11 +1,19 @@
 import Image from "next/image";
 import React from "react";
+import jujutsu from "@/assets/jujutsu.jpg";
 
 const Album = () => {
   return (
     <div className=" px-6 md:px-8 lg:px-40">
       <h1>Using image component from next js</h1>
       <Image
+        width={1500}
+        height={500}
+        src={"https://i.ibb.co/FHy1DgY/Firefly-20240105224119.jpg"}
+        alt="bg-image"
+      />
+      <Image width={1500} height={500} src={jujutsu} alt="bg-image" />
+      <img
         width={1500}
         height={500}
         src={"https://i.ibb.co/FHy1DgY/Firefly-20240105224119.jpg"}
